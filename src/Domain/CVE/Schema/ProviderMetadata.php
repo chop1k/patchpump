3330631cd846398de1395b57331b227be315cf19
chop1k <goalbuff@yandex.ru>
2025-06-15 17:07:58 +0300

    added rules for rejected CNA

--- src/Domain/CVE/Schema/ProviderMetadata.php
+++ src/Domain/CVE/Schema/ProviderMetadata.php
@@ -6,6 +6,18 @@ namespace App\Domain\CVE\Schema;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * Class that represents CVE provider metadata object according to MITRE CVE V5 schema.
+ *
+ * Provider metadata contains information about vulnerability information provider organization.
+ *
+ * Objects of the class are used for validating CVE schema and serialization/deserialization.
+ *
+ * @link https://github.com/CVEProject/cve-schema
+ * @link https://github.com/CVEProject/cve-schema/blob/main/schema/docs/CVE_Record_Format_bundled.json
+ *
+ * @see CNA
+ */
 final class ProviderMetadata
 {
     #[Assert\NotNull]
