05f0b3709b283e14af95f062124b3ef425ec7b2b
chop1k <goalbuff@yandex.ru>
2025-07-30 22:25:50 +0300

    feat(synchronization): added file source implementation for cve records synchronization

--- src/Domain/CVE/Synchronization/Source/FileSource.php
+++ src/Domain/CVE/Synchronization/Source/FileSource.php
@@ -4,18 +4,35 @@ declare(strict_types=1);
 
 namespace App\Domain\CVE\Synchronization\Source;
 
+use App\Domain\CVE\Synchronization\Source\Common\Factory;
 use App\Domain\Vulnerabilities\Synchronization\Contracts\SourceInterface;
 use App\Persistence\Document\CVE\Record;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * @implements SourceInterface<Record>
  */
-final class FileSource implements SourceInterface
+final readonly class FileSource implements SourceInterface
 {
-    private bool $located = false;
+    public function __construct(
+        private Filesystem $filesystem,
+        private Factory $factory,
+        private string $path,
+    ) {
+    }
 
+    /**
+     * @return \Generator<string, Record>
+     */
     public function generator(): \Generator
     {
-        throw new \BadMethodCallException('Not implemented');
+        $text = $this->filesystem->readFile($this->path);
+
+        $record = $this->factory->record($text)
+            ->serialize()
+            ->validate()
+            ->toPersistence();
+
+        yield $record->getId() => $record;
     }
 }
